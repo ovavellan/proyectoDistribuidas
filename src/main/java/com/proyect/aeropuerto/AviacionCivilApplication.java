fb7f18d9d19dea16b9d7d7c25498895201b4837a
Katherine-Bravo-Morillo <105561336+Katherine-Bravo-Morillo@users.noreply.github.com>
2023-12-20 06:58:04 -0500

    Update AviacionCivilApplication.java

--- src/main/java/com/proyect/aeropuerto/AviacionCivilApplication.java
+++ src/main/java/com/proyect/aeropuerto/AviacionCivilApplication.java
@@ -7,7 +7,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AviacionCivilApplication {
 
 	public static void main(String[] args) {
+		
+	System.out.println("******************************************");
+	     System.out.println("APLICACIONES DISTRIBUIDAS ");
+             System.out.println("Bienvenidos Sistema de Aviacion");
+	  System.out.println("******************************************");
+		
 		SpringApplication.run(AviacionCivilApplication.class, args);
 	}
+
+	
 
 }
